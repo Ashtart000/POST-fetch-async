7fdc6be79477bb6597216f19ff294d271a7abf7f
Anna Lednyova <ashtart@gmail.com>
2023-08-14 20:08:22 +0300

    POST fetch

---
+++ index.js
@@ -0,0 +1,25 @@
+const API_BASE = 'https://fakestoreapi.com/products';
+
+const product = {
+    title: 'Huawei PS120+',
+    price: 100500,
+    description: 'Telephone',
+    image: 'https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/pdp/admin-image/phones/nova9-se/list/list-blue.png',
+    category: 'Phones',
+    rating: {
+        rate: 5,
+        count: 24
+    }
+}
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(product)
+}
+
+fetch(API_BASE, options)
+.then((response) => response.json())
+.then((data) => console.log(data))
